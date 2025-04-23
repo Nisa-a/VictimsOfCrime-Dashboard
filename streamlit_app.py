5ebe07144cc71ee774a5df6bd37d629cdc030c8d
Anisa <nisaamroun@gmail.com>
2025-04-23 15:42:09 +0000

    Remove "Region" as there is only one region

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,29 +9,28 @@ df = pd.read_csv("cleaned_data_crime.csv")
 # Titles:
 st.title("Crime Victimisation")
 st.markdown("An interactive dashboard")
-st.caption("Use the filters in the sidebar to explore how victims of crime varies by year, region, and demographics.")
+st.caption("Use the filters in the sidebar to explore how victims of crime varies by year, gender, and demographics.")
 st.markdown("---")
+
 # Sidebar:
 st.sidebar.title("Filter Options")
 st.sidebar.markdown("---")
+
 # Filter widgets:
 # Wa, W. (2020) Build your first interactive dashboard with cross-filtering in Streamlit. Medium. Available at: https://medium.com/@weijiawa/build-your-first-interactive-dashboard-with-cross-filtering-in-streamlit-e7ae673001d3 (Accessed: 22 April 2025).
 year_filter = st.sidebar.multiselect("Year", sorted(df["Year"].unique()), default=["2013/14","2014/15","2015/16","2016/17","2017/18","2018/19","2019/20"])
-region_filter = st.sidebar.multiselect("Region", df["Region"].unique())
 ethnicity_filter = st.sidebar.multiselect("Ethnicity", sorted(df["Ethnicity"].unique()))
 gender_filter = st.sidebar.selectbox("Gender", sorted(df["Gender"].unique()), index = 0)
 age_filter = st.sidebar.multiselect("Age", df["Age"].unique())
 socio_eco_class_filter = st.sidebar.multiselect("Socio-economic classification", df["Socio-economic classification"].unique())
 house_income_filter = st.sidebar.multiselect("Household income", df["Household income"].unique())
 st.sidebar.markdown("---")
+
 # Apply filters only if a value is selected:
 # Wa, W. (2020) Build your first interactive dashboard with cross-filtering in Streamlit. Medium. Available at: https://medium.com/@weijiawa/build-your-first-interactive-dashboard-with-cross-filtering-in-streamlit-e7ae673001d3 (Accessed: 22 April 2025).
 filtered_data = df.copy()
 if year_filter:
     filtered_data = filtered_data[filtered_data["Year"].isin(year_filter)]
-
-if region_filter:
-    filtered_data = filtered_data[filtered_data["Region"].isin(region_filter)]
 
 if ethnicity_filter:
     filtered_data = filtered_data[filtered_data["Ethnicity"].isin(ethnicity_filter)]
@@ -54,18 +53,15 @@ if filtered_data.empty:
 
 # Create download button for filtered data.
 st.sidebar.download_button("Download Filtered Data as CSV", filtered_data.to_csv(index=False), "filtered_data.csv")
-# Layout for the summary metrics:
-# Only the single years are needed to display the metrics.
-trend_years = ['2013/14', '2014/15', '2015/16', '2016/17', '2017/18', '2018/19', '2019/20']
-trend_data = df[df["Year"].isin(trend_years)]
 
+# Layout for the summary metrics:
 # Tech with Tim (2021) How to Build an Interactive Dashboard with Streamlit. YouTube. Available at: https://www.youtube.com/watch?v=hQnMV_bF84I (Accessed: 22 April 2025).
 # Streamlit (n.d.) st.metric – Streamlit Docs. Available at: https://docs.streamlit.io/develop/api-reference/data/st.metric (Accessed: 22 April 2025).
 # Streamlit (n.d.) st.columns – Streamlit Docs. Available at: https://docs.streamlit.io/develop/api-reference/layout/st.columns (Accessed: 22 April 2025).
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    highest_year = trend_data.groupby("Year")["Sample size"].sum().idxmax()
+    highest_year = df.groupby("Year")["Sample size"].sum().idxmax()
     col1.metric("Year with Most Reported Victims", highest_year)
 
 with col2:
@@ -73,10 +69,10 @@ with col2:
     col2.metric("Covered Regions", region_count)
 
 with col3:
-    average_rate = round(trend_data["Value"].mean(), 2)
+    average_rate = round(df["Value"].mean(), 2)
     col3.metric("Overall Victimisation Rate", f"{average_rate:.2f}%")
 
-# Trend ine chart 
+# Trend line chart: 
 victim_trend_chart = filtered_data.groupby("Year")["Sample size"].sum().reset_index()
 fig = px.line(victim_trend_chart, x="Year", y="Sample size", labels={"Sample size": "Victim Count"}, title="Number of Victims Over Time")
 st.plotly_chart(fig)
@@ -87,7 +83,7 @@ if st.checkbox("Show Raw Filtered Data"):
 
 # Create tabs for the different visualisations:
 # Streamlit (n.d.) st.tabs – Streamlit Docs. Available at: https://docs.streamlit.io/develop/api-reference/layout/st.tabs (Accessed: 22 April 2025).
-tab1, tab2, tab3 = st.tabs(["By Ethnicity", "By Region", "By Age Group"])
+tab1, tab2, tab3 = st.tabs(["By Ethnicity", "By Gender", "By Age Group"])
 
 # Bar chart for ethnicity:
 ethnicity_stats = filtered_data.groupby("Ethnicity")["Sample size"].mean().reset_index()
@@ -96,3 +92,5 @@ fig = px.bar(ethnicity_stats, height = 600,width = 900, x="Ethnicity", y="Sample
 # plotly.com. (n.d.). Layout.xaxis. [online] Available at: https://plotly.com/python/reference/layout/xaxis/.
 fig.update_xaxes(showticklabels=False) # Hide x-axis label.
 tab1.plotly_chart(fig)
+
+
