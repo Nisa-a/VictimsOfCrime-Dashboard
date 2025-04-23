194ca4bea7b2dc1849bee5a924728492f8b2c3ad
Anisa <nisaamroun@gmail.com>
2025-04-23 15:15:53 +0000

    Designing the title layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,14 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
+# Loading dataset into a data frame.
+df = pd.read_csv("cleaned_data_crime.csv")
+
+# Titles:
+st.title("Crime Victimisation")
+st.markdown("An interactive dashboard")
+st.caption("Use the filters in the sidebar to explore how victims of crime varies by year, region, and demographics.")
+st.markdown("---")
+# Sidebar:
+st.sidebar.title("Filter Options")
+st.sidebar.markdown("---")
